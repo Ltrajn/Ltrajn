4c0f02161b9a4690101105741745dc3ee7f21da1
Ltrajn <95147399+Ltrajn@users.noreply.github.com>
2021-12-26 15:04:29 -0600

    Update HexFormatException.java

--- HexFormatException.java
+++ HexFormatException.java
@@ -1,3 +1,5 @@
+//Custom Exception Class which accompanies Hex2dec.java file, simmple enough.
+
 public class HexFormatException extends Exception{
   public HexFormatException(char ex){
     System.out.println("Illegal hex character");
